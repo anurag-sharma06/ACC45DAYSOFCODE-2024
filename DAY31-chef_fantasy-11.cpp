5cdef93235de487e25052e2c744e7c64cc4fa6d7
anurag-sharma06 <sanuragsharma001@gmail.com>
2024-11-02 22:21:03 +0530

    Add files via upload

---
+++ DAY31-chef_fantasy-11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int t, c;
+    cin>>t;
+    while(t--){
+        cin>>c;
+        cout<<(c*c)-c<<endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
